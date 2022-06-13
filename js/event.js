d265de2ca3c927c066ca73dccbe2556e3e6b1926
VOVAN <plategi@yandex.ru>
2022-06-13 21:35:14 +0300

    home

---
+++ js/event.js
@@ -0,0 +1,12 @@
+console.log("Вася привет ");
+
+setTimeout(function(){
+    console.log('Hi SetTimeout');
+}, 3000);
+
+console.log("Вася пока");
+
+let a = 4;
+let b = 2;
+let result = a / b;
+console.log("Результат деления: ", result);
